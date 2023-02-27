7bf69369800630b38be667c55f67936bb03f3631
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-02-27 13:07:38 +0530

    typechecking using prop-typesn package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Demo from "./components/Demo";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Demo name="hello">
+        <h1>hello , i'm children of Demo Component</h1>
+        <h2>dskjhdsjhj</h2>
+      </Demo>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Types from "prop-types";
+const Demo = props => {
+  console.log(props);
+  return (
+    <div>
+      Demo
+      <h1>Name : {props.name}</h1>
+    </div>
+  );
+};
 
-const Demo = () => {
-  return <div>Demo</div>;
+Demo.defaultProps = {
+  name: "No User",
+};
+
+Demo.propTypes = {
+  name: Types.string,
 };
 
 export default Demo;
