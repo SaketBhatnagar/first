745e7ffbfce85565063e25f163f24147a8dd0f81
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-02-27 12:37:39 +0530

    demo  component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Search from "./components/Search";
-import { data } from "./assests/Data";
+
 const App = () => {
-  return (
-    <>
-      <Search data={data} />
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
