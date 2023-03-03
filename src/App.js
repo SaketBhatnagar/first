71743a01b75d1bb5e1f13795ad1f39209d2c9dfd
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-03-03 12:25:07 +0530

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import Demo from "./components/Demo";
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      remove: false,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ remove: !this.state.remove });
+    }, 5000);
   }
   render() {
     return (
       <div>
         <h1 className="text-[10vw]"> App</h1>
-        <Demo name="chomkdlkkhdjslkhjkdslkhjddsiuiudgdsksdk" />
+
+        {this.state.remove ? <Demo></Demo> : "hello"}
       </div>
     );
   }
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,31 +1,54 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class Demo extends Component {
-  constructor(props) {
+export default class Demo extends Component {
+  constructor() {
     super();
-    this.name = props.name;
     this.state = {
-      name: "chombu",
+      name: "chombi",
     };
-    this.count = 0;
+    console.log("constructor");
+  }
+
+  static getDerivedStateFromProps() {
+    console.log("getDerivedStateFromProps");
   }
 
   componentDidMount() {
-    // this.state.name = "sjhsjgsj";
-    this.setState({ name: "chombu22222" });
-    this.setState({ name: "chombu2289768776" });
-    console.log("componentDidmount running.....");
+    console.log("componentDidMount");
+    // setInterval(() => {
+    //   this.setState({ name: this.state.name + 1 });
+    // }, 6000);
+  }
+
+  componentWillUnmount() {
+    console.log("component removed from Real DOM");
+    alert("removed");
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("component updated");
   }
   render() {
-    console.log("render running");
+    console.log("render");
     return (
-      <>
-        <h1>Demo Class Component</h1>
-        <h2>{this.name}</h2>
-        <h2>{this.state.name}</h2>
-      </>
+      <div>
+        <form>
+          Name <input type="text" className="border-spacing-3" />
+          <br />
+          Password <input type="text" className="border-spacing-3" />
+          <br />
+          <button>Submit</button>
+        </form>
+      </div>
     );
   }
 }
-
-export default Demo;
