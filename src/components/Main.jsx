4729d45b69104b9ef8079a0cc79c867c45bd6f7a
Saket Bhatnagar <bhatnagarsaket7367@gmail.com>
2023-03-17 14:46:09 +0530

    useState hooks counter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,47 +1,34 @@
-import React, { Component } from "react";
+import { UseState, useState } from "react";
+const Main = () => {
+  let [count, setCount] = useState(0);
 
-export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      name: "mojombo",
-    };
-  }
+  let handleIncrement = () => {
+    setCount(prev => prev + 1);
+    // setCount(count + 1);
+  };
+  let handleDecrement = () => {
+    setCount(prev => prev - 1);
+    // setCount(count - 1);
+  };
+  let handleReset = () => {
+    setCount(prev => prev * 0);
+    // setCount(0);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDecrement}>-</button>
+    </div>
+  );
+};
 
-  componentDidMount() {
-    try {
-      fetch(`https://api.github.com/users/${this.state.name}`).then(data =>
-        data.json().then(res => {
-          console.log(res);
-          this.setState({ data: res });
-        })
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  render() {
-    return (
-      <div>
-        {/* {this.state.data.map(value => {
-          return (
-            <div key={value.id}>
-              <img src={value.avatar_url} alt={value.login} width="300px" />
-              <h1>{value.login}</h1>
-            </div>
-          );
-        })} */}
+export default Main;
 
-        {/* {this.state.data.map(value => {
-          return (
-            <div key={value.id} className="card">
-              <h2>Name :{value.name}</h2>
-              <h2>Email :{value.email}</h2>
-            </div>
-          );
-        })} */}
-      </div>
-    );
-  }
-}
+// Ṛeact Provides 'useState' hook to implement state inside function components
+// By using state hook , we store any type of literal
+//
+// useState  returns array of satte variable and it's function to update the state and re-render UI after state update;
+// To use state and function , we have destructure it
+// to set default value of state , we can pass value to useState("value") ;
